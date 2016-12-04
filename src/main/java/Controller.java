3122d0778b71fcd7f82a7957087a14eabefca8ad
Isaac Enmanuel Perez <20130824@ce.pucmm.edu.do>
2016-12-03 21:06:32 -0400

    lk

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -24,7 +24,9 @@ import java.nio.file.StandardCopyOption;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
-
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
 
 import static spark.Spark.*;
 
@@ -373,7 +375,7 @@ public class Controller {
             response.type("text/html");
             HashMap model = new HashMap();
 
-            Path tempFile = Files.createTempFile(uploadDir.toPath(), "file ", ".png");
+            Path tempFile = Files.createTempFile(uploadDir.toPath(), "", ".jpg");
 
             request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
 
@@ -382,7 +384,10 @@ public class Controller {
                 Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
             }
 
+
             System.out.println(tempFile.getFileName());
+            enviarCorreo("isaacenmanuel28@gmail.com", "info@acreser", "Naranja1234", tempFile.toAbsolutePath().toString( ) );
+
             logInfo(request, tempFile);
             return new ModelAndView(model, "templates/enviarPromocion.html");
 
@@ -403,5 +408,54 @@ public class Controller {
         return null;
     }
 
+    private static void enviarCorreo(String destino, String usuario, String clave, String attachment){
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", "mail.javatpoint.com");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(usuario,clave);
+                    }
+                });
+
+        //2) compose message
+        try{
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(usuario));
+            message.addRecipient(Message.RecipientType.TO,new InternetAddress(destino));
+            message.setSubject("Message Aleart");
+
+            //3) create MimeBodyPart object and set your message text
+            BodyPart messageBodyPart1 = new MimeBodyPart();
+            messageBodyPart1.setText("This is message body");
+
+            //4) create new MimeBodyPart object and set DataHandler object to this object
+            MimeBodyPart messageBodyPart2 = new MimeBodyPart();
+
+            DataSource source = new FileDataSource(attachment);
+            messageBodyPart2.setDataHandler(new DataHandler(source));
+            messageBodyPart2.setFileName(attachment);
+
+
+            //5) create Multipart object and add MimeBodyPart objects to this object
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart1);
+            multipart.addBodyPart(messageBodyPart2);
+
+            //6) set the multiplart object to the message object
+            message.setContent(multipart );
+
+            //7) send message
+            Transport.send(message);
+
+            System.out.println("message sent....");
+        }catch (MessagingException ex) {ex.printStackTrace();}
+    }
+
 }
+
+
 
